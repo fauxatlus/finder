d403a8265715ebb66637a79664d45c0d7dab64b6
Ex Novo <randy.a.littlefield@gmail.com>
2018-08-11 23:18:53 -0400

    adding in homework

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Randy Littlefield',
+    profilePic: 'https://imgur.com/0c32sya',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
